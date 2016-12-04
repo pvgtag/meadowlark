249b39c8a60391b474b984a4741b882a21a24930
Vadim Petrov <pvg.tag@yandex.ru>
2016-12-04 13:18:20 +0300

    Add handlebars and lib

--- meadowlark.js
+++ meadowlark.js
@@ -1,33 +1,40 @@
-var express = require('express'),
-    app = express();
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars')
+	.create({ defaultLayout:'main' });
+var fortune = require('./lib/fortune');   
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('Meadoelark Trevel');
+	res.render('home');
 });
 
-app.get('/about', function(req, res) {
-    res.type('text/plain');
-    res.send('About Meadoelark Trevel');
+app.get('/about', function(req,res){
+	// var randomFortune = 
+	// 	fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
+	res.render('about', {fortune: fortune.getFortune()});
 });
 
-
-app.use(function(req, res) {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Не найдено');
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
 });
 
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - Server error');
-    
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
 });
 
-app.listen(3000, function() {
-    console.log('App listening on port ' + app.get('port') + '!');
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
 });
